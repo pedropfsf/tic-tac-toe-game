096541ddf535b65a902d437b1663f59723f2207a
Pyedrown <Pedrinho_fsf@hotmail.com>
2022-04-16 09:19:56 -0300

    :wrench: chore: Programado as cores e adicionados os svgs para o app
    - :heavy_plus_sign: build: Adicionado a lib styled-components

---
+++ src/styles/colors.ts
@@ -0,0 +1,10 @@
+const colors = {
+  playerX: '#46A6FF',
+  playerO: '#FF465C',
+  detach: '#6246FF',
+  background: '#060029',
+  secundary: '#E8E5F9',
+  success: '#51EE4E'
+};
+
+export default colors;
