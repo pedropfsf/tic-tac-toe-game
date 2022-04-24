b29a8b45be3395fe65fbd4ea1b033bedfc0d8712
Pyedrown <Pedrinho_fsf@hotmail.com>
2022-04-24 15:06:30 -0300

    :sparkles: feat: Criado um rota do Home que volta para tela de Seleção de jogadores

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,3 +1,9 @@
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import RootNativeStackParamsList from '../../types/RootNativeStackParamsList';
+
+type HomeScreenNavigationProp = NativeStackNavigationProp<RootNativeStackParamsList, 'Home'>;
+
 import { 
   AreaScreen, 
   Container, 
@@ -9,8 +15,16 @@ import { StatusPlayer, Text, Button } from '../../components';
 import { ImageIconLogo } from '../../assets';
 import Logo from '../../components/Logo';
 import colors from '../../styles/colors';
+import { useContextGame } from '../../contexts/GameContext';
 
 function HomeScreen() {
+  const navigation = useNavigation<HomeScreenNavigationProp>();
+  const { state } = useContextGame();
+
+  function goSelectPlayers() {
+    navigation.navigate('SelectPlayers');
+  }
+
   return (
     <AreaScreen>
       <Logo
@@ -20,28 +34,33 @@ function HomeScreen() {
       <Container>
         <AreaStatusPlayers>
           <StatusPlayer
-            namePlayer='Pedro'
-            quantityVitory={2}
+            namePlayer={state.playerX.name}
+            quantityVitory={state.playerX.victories}
             typePlayer='Jogador X'
             color={colors.playerX}
           />
           <Text 
             color={colors.detach}
             margin="8px"
-          >VS</Text>
+          >
+            VS
+          </Text>
           <StatusPlayer
-            namePlayer='Tiago'
-            quantityVitory={2}
+            namePlayer={state.playerO.name}
+            quantityVitory={state.playerO.victories}
             typePlayer='Jogador O'
             color={colors.playerO}
           />
         </AreaStatusPlayers>
         <AreaButtons>
-          <Button optionsButton={{
-            colorButton: 'transparent',
-            BorderButton: `2.5px solid ${colors.secundary}`,
-            marginBottomButton: 16
-          }}>
+          <Button 
+            optionsButton={{
+              colorButton: 'transparent',
+              BorderButton: `2.5px solid ${colors.secundary}`,
+              marginBottomButton: 16
+            }}
+            onPress={goSelectPlayers}
+          >
             Voltar
           </Button>
           <Button>
